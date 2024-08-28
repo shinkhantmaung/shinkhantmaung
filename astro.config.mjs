f206709e937305f18c325d9a8700c87fa27c423f
Shin Khant Maung <shinkhantmaungs@gmail.com>
2024-08-28 16:24:00 +0630

    fix: resolve readingTime calculation bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
 
 
 // https://astro.build/config
@@ -10,6 +11,7 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt()],
   site: SITE_URL,
   markdown: {
+    remarkPlugins: [remarkReadingTime],
     syntaxHighlight: "shiki",
     shikiConfig: {
       theme: "nord",
---
+++ src/content/writings/untitled.md
@@ -0,0 +1,10 @@
+---
+title: Untitled
+image: 
+publishedAt: 2023-05-24
+description: Lorem ipsum dolor sit amet consectetur et ultrices blandit neque ege
+slug: untitled
+isPublish: false
+---
+
+## Hello World
