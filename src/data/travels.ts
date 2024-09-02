9b6a72ba0ecd36adbe38402c7f16c49aae37f780
Shin Khant Maung <shinkhantmaungs@gmail.com>
2024-09-02 17:24:32 +0630

    feat: grouped the countru in travels

--- src/data/travels.ts
+++ src/data/travels.ts
@@ -9,132 +9,165 @@ export type Travel = {
     {
         title: 'Mandalay',
         imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/o6G5F4yScF6YvXB7-IMG_0255.JPG?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Pakokku',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6stQMYl0BGoAym8f-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6stQMYl0BGoAym8f-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Sagaing',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/TsQgVPFshcZRglIx-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/TsQgVPFshcZRglIx-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Thaton',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/mrMLYC6LvMJV02YX-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/mrMLYC6LvMJV02YX-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Bagan',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/akrGPt2YXr4Xo1n9-IMG_0172.JPEG?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/akrGPt2YXr4Xo1n9-IMG_0172.JPEG?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Dala',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/pMKVh8V792N7uUKf-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/pMKVh8V792N7uUKf-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Pyin Oo Lwin',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/REQRavVnk1QT7RhN-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/REQRavVnk1QT7RhN-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Chaung Thar',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/h8sxdwRNuIgw2Wuq-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/h8sxdwRNuIgw2Wuq-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Heho',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6pvM43P7s3gCsdni-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6pvM43P7s3gCsdni-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Laukkaing',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/ipJk1nFxOB3JiG3N-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/ipJk1nFxOB3JiG3N-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Thanlyin',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/SsetIxnfWg5Nbmnz-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/SsetIxnfWg5Nbmnz-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Hsipaw',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/KPBnCJtiLlpUksEl-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/KPBnCJtiLlpUksEl-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Taungoo',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6mZMApz9XQ4oI9NB-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6mZMApz9XQ4oI9NB-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Kunlong',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/BYSw4iBR2irsRoQ9-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/BYSw4iBR2irsRoQ9-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Kyaukse',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/0rgojMofjzDPb1dZ-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/0rgojMofjzDPb1dZ-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Lashio',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/otEcOp0nyNINAELV-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/otEcOp0nyNINAELV-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Mya Lake',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/q6Y8SXnARdLjVIpx-1000024033.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/q6Y8SXnARdLjVIpx-1000024033.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Pinlaung',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/7UIk2WGINnHq8J29-1000024031.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/7UIk2WGINnHq8J29-1000024031.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Ngwe Saung',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/VCjURmgC8EbJhqr3-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/VCjURmgC8EbJhqr3-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Nyaungshwe',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/UiaXdYV0DA0yRp5R-1000020265.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/UiaXdYV0DA0yRp5R-1000020265.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Chinshwehaw',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/DIcShIxiAoIHRRt2-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/DIcShIxiAoIHRRt2-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Taaikkyi',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/88Empohdmopk4ESE-1000022214.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/88Empohdmopk4ESE-1000022214.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Amarapura',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6f8QA0LEBFBJQnPy-1000018944.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/6f8QA0LEBFBJQnPy-1000018944.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Magway',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/SceOc9jYEj1n3FOV-1000024032.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/SceOc9jYEj1n3FOV-1000024032.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Hpa-na',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/fKQFJbSP2SXo119R-1000024029.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/fKQFJbSP2SXo119R-1000024029.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Thandaunggyi',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/rHhIAceSEcekr5Qw-1000024030.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/rHhIAceSEcekr5Qw-1000024030.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Aungban',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/7jgk1yn91Wnr3avf-1000018537.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/7jgk1yn91Wnr3avf-1000018537.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Hlawga',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/30i2TxoJrlb1rQIB-image.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/30i2TxoJrlb1rQIB-image.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Pindaya',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/QYqLaTgdp7OzneqY-vlcsnap-2024-08-31-09h46m17s595.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/QYqLaTgdp7OzneqY-vlcsnap-2024-08-31-09h46m17s595.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Kalaw',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/EncUFfKETb2rZ8ND-1000020130.jpg?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/EncUFfKETb2rZ8ND-1000020130.jpg?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Nay Pyi Taw',
-        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/1n6ymuY7WhKEn5R9-vlcsnap-2024-08-31-09h45m35s622.png?w=750&h=750'
+        imageSrc: 'https://creatorspace.imgix.net/users/clk3g7grl0a5ep101n228b8fv/1n6ymuY7WhKEn5R9-vlcsnap-2024-08-31-09h45m35s622.png?w=750&h=750',
+        country: 'Myanmar'
     },
     {
         title: 'Kyaikto',
+        country: 'Myanmar'
     },
     {
-        title: 'Mawlamyine'
+        title: 'Mawlamyine',
+        country: 'Myanmar'
     }
   ];
   
